780e40208c6b01dbc3e40cab7357bd0eb0edd371
Guerau <44753538+GuerauO@users.noreply.github.com>
2022-07-05 22:53:47 +0200

    Update ballBehaviour.cs

--- ballBehaviour.cs
+++ ballBehaviour.cs
@@ -5,29 +5,17 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class ballBehaviour : MonoBehaviour
 {
-    // Start is called before the first frame update
-    Rigidbody rb;
-    public float maxVelocity = 10.0f;
 
-    void Start()
-    {
-        rb = GetComponent<Rigidbody>();
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        if(rb.velocity.y > maxVelocity)
-        {
-            rb.velocity = new Vector3(0, maxVelocity, 0);
-        }
-    }
+    public Rigidbody rb;
+    public float bounce = 6;
 
     private void OnCollisionEnter(Collision collision)
     {
+        rb.velocity = new Vector3(rb.velocity.x, bounce, rb.velocity.z);
+
         if(collision.gameObject.tag == "ENEMY")
         {
-            transform.SetPositionAndRotation(new Vector3(2.5f, 10.0f, 0.0f), Quaternion.identity);
+            transform.SetPositionAndRotation(new Vector3(-2f, 20.0f, 0.0f), Quaternion.identity);
             rb.velocity = new Vector3(0, 2, 0);
         }
         
