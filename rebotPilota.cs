7bbc9096066c2a213183247de5c63f43d20d2280
Guerau <44753538+GuerauO@users.noreply.github.com>
2022-07-06 09:34:16 +0200

    Add files via upload

---
+++ rebotPilota.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.SceneManagement;
+
+public class rebotPilota : MonoBehaviour
+{
+    public Rigidbody rb;
+    public float rebot = 6;
+    
+    private void OnCollisionEnter(Collision collision)
+    {
+        rb.velocity = new Vector3(rb.velocity.x, rebot, rb.velocity.z);
+
+    }
+
+
+}
