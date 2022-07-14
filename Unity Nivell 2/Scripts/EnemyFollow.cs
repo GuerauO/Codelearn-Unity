b575abaa2d285ee978fb58715faf5f5895228b9b
Guerau <44753538+GuerauO@users.noreply.github.com>
2022-07-14 13:11:27 +0200

    Update EnemyFollow.cs

--- Unity Nivell 2/Scripts/EnemyFollow.cs
+++ Unity Nivell 2/Scripts/EnemyFollow.cs
@@ -1,25 +1,50 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UniyEngine.AI
+using UnityEngine.AI;
+using System;
 
-public class EnemyFollow : MonoBehaviour
+
+public class Perseguir : MonoBehaviour
 {
-
-
-    public UnityEngine.AI.NavMeshAgent enemy;
+    public NavMeshAgent enemy;
     public Transform Player;
 
+    public Transform Enemy;
 
-    // Start is called before the first frame update
-    void Start()
+
+    public float llindar;
+
+    
+
+    private void OnTriggerEnter(Collider other)
     {
-        
+        enemy.speed = 3;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        enemy.speed = 30;
     }
 
     // Update is called once per frame
     void Update()
     {
+
+
+       
+        if (Math.Abs(Enemy.position.x - Player.position.x) >= llindar ||
+            Math.Abs(Enemy.position.y - Player.position.y) >= llindar ||
+            Math.Abs(Enemy.position.z - Player.position.z) >= llindar)
+        {
+            enemy.speed = 30;
+
+        }
+        else
+            enemy.speed = 3;
+
+
         enemy.SetDestination(Player.position);
+
     }
 }
