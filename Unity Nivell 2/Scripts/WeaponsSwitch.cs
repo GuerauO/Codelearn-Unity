a9ef443f224089d602aaae0bd432f01d0369a8c5
Guerau <44753538+GuerauO@users.noreply.github.com>
2022-07-15 09:33:25 +0200

    Update WeaponsSwitch.cs

--- Unity Nivell 2/Scripts/WeaponsSwitch.cs
+++ Unity Nivell 2/Scripts/WeaponsSwitch.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -8,6 +8,9 @@ public class WeaponsSwitch : MonoBehaviour
     public GameObject object02;
     public GameObject object03;
 
+    public Inventory inventari;
+
+    
 
 
     void Start()
@@ -15,6 +18,7 @@ public class WeaponsSwitch : MonoBehaviour
         object01.SetActive(false);
         object02.SetActive(false);
         object03.SetActive(false);
+
     }
 
 
@@ -29,21 +33,21 @@ public class WeaponsSwitch : MonoBehaviour
             object03.SetActive(false);
         }
 
-        if (Input.GetButtonDown("2"))
+        if (Input.GetButtonDown("2") && inventari.item1 == true)
         {
             object01.SetActive(true);
             object02.SetActive(false);
             object03.SetActive(false);
         }
 
-        if (Input.GetButtonDown("3"))
+        if (Input.GetButtonDown("3") && inventari.item2 == true)
         {
             object01.SetActive(false);
             object02.SetActive(true);
             object03.SetActive(false);
         }
 
-        if (Input.GetButtonDown("4"))
+        if (Input.GetButtonDown("4") && inventari.item3 == true)
         {
             object01.SetActive(false);
             object02.SetActive(false);
